8bc06f65d7ad68b1aaf15f5f7db7d4b95a23c4f8
Hadrien Kohl <hadrien.kohl@gmail.com>
2019-09-16 15:59:16 +0200

    Remove logback require in module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module no.ssb.lds.gsim.okhttp {
     requires com.fasterxml.jackson.databind;
     requires okhttp3;
-    requires static ch.qos.logback.classic;
     requires org.slf4j;
     requires com.fasterxml.jackson.annotation;
 
