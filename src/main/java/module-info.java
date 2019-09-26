67fd408125e3dbce5b382ddfaa4af5ff3b088cb4
Hadrien Kohl <hadrien.kohl@gmail.com>
2019-09-26 13:27:59 +0200

    Prepare project to use gitflow

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module no.ssb.lds.gsim.okhttp {
-    requires com.fasterxml.jackson.databind;
-    requires okhttp3;
-    requires org.slf4j;
-    requires com.fasterxml.jackson.annotation;
-
-    exports no.ssb.lds.gsim.okhttp;
-}
